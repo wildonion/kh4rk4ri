e31f0dbe67f158e3e4c39b0499f3c265c2cb9879
wildonion <marefimoghaddam@unixerr.com>
2021-04-30 04:35:35 -0700

    add . to import Preprocessor class

--- utils/_dataloader.py
+++ utils/_dataloader.py
@@ -4,13 +4,14 @@
 
 import pandas as pd
 import numpy as np
-from _preprocessor import Preprocessor
+from ._preprocessor import Preprocessor
 
 
 __all__ = ["DataLoader"]
 
 class DataLoader:
     def __init__(self, preprocessed: Preprocessor):
+        # TODO - split: float = 0.8, limit: int = 0
         self.__preprocessed = preprocessed
         self.x_train        = self.__preprocessed.x_train
         self.y_train        = self.__preprocessed.y_train
