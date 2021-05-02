06491bfaf4d5860c0e3b9df4b546b8b61d6f54b1
wildonion <marefimoghaddam@unixerr.com>
2021-05-02 09:28:43 +0430

    add vectorizer method and return the pipeline inside the __call__ method for the models

--- utils/_dataloader.py
+++ utils/_dataloader.py
@@ -2,19 +2,31 @@
 
 
 
-import pandas as pd
-import numpy as np
+
+
 from ._preprocessor import Preprocessor
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 __all__ = ["DataLoader"]
 
 class DataLoader:
-    def __init__(self, preprocessed: Preprocessor):
-        # TODO - split: float = 0.8, limit: int = 0
-        self.__preprocessed = preprocessed
-        self.x_train        = self.__preprocessed.x_train
-        self.y_train        = self.__preprocessed.y_train
+    def __init__(self, preprocessed: Preprocessor, transformer: Preprocessor.Transformer):
+        self.preprocessed = preprocessed
+        self.transformer  = transformer
 
-    def __call__(self):
-        pass
+
+    def __call__(self, classifier):
+        pipe = Pipeline([("cleaner", self.transformer()),
+                         ('vectorizer', self.vectorizer()),
+                         ('classifier', classifier)])
+        return pipe
+
+
+    def vectorizer(self):
+        # NOTE - in BOW we'll have frequencies, rather than just 1 or 0 for their occurrence in ngram.
+        self.bag_of_words = CountVectorizer(tokenizer=self.preprocessed.tokenizer, ngram_range=(1,1))
+        # NOTE - we're using BOW with TF-IDF normalization for building our vocabulary and feature extraction.
+        self.tfidf_vector = TfidfVectorizer(tokenizer=self.preprocessed.tokenizer)
+        return self.tfidf_vector
