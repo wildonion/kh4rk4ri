4e5c0733856865e16fe05750ed0984a83d06e0da
wildonion <marefimoghaddam@unixerr.com>
2021-05-02 09:27:39 +0430

    add train function body to train data through the pipeline of dataloader object

--- models/_base.py
+++ models/_base.py
@@ -1,16 +1,24 @@
 
 
 
+# https://towardsdatascience.com/text-classification-using-naive-bayes-theory-a-working-example-2ef4b7eb7d5a
+# https://www.dataquest.io/blog/tutorial-text-classification-in-python-using-spacy/
 
+
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 
 class BaseLine:
     def __init__(self, *args, **kwargs):
         self.dataloader = kwargs["dataloader"]
+        self.model      = kwargs["model"]
 
     def stat(self):
-        # TODO - classification confusion matrix and scores
         pass
 
     def __call__(self):
         pass
+
+    def train(self):
+        pipe = self.dataloader(self.model)
+        pipe.fit(self.dataloader.x_train, self.dataloader.y_train)
