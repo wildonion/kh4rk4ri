ded0fd2741b1e377d742bd6ffff54f2b41e7d0bf
wildonion <marefimoghaddam@unixerr.com>
2021-05-02 23:58:52 +0430

    move Transformer class to the outside of the Preprocessed class and add vectorizer method in here

--- utils/_preprocessor.py
+++ utils/_preprocessor.py
@@ -12,28 +12,15 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.lang.en import English
 from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 spacy.load('en')
 
 
 
-__all__ = ["Preprocessor"]
+__all__ = ["Preprocessor", "Transformer"]
 
 
 class Preprocessor:
-    class Transformer(TransformerMixin):
-        def transform(self, doc, **transform_params):
-            return [self.clean_text(text) for text in doc]
-
-        def fit(self, doc, y=None, **fit_params):
-            return self
-
-        def get_params(self, deep=True):
-            return {}
-
-        def clean_text(self, text):
-            return text.strip().lower()
-
-
     def __init__(self, paths: List[str]):
         if not os.path.exists(paths[0]): print("[?] CSV Datasets Not Found!"); sys.exit(1)
         self.__data_path    = f"{paths[0]}/x_train.csv"
@@ -59,3 +46,24 @@ class Preprocessor:
         return tokens
         
 
+    def vectorizer(self):
+        # NOTE - in BOW we'll have frequencies, rather than just 1 or 0 for their occurrence in ngram.
+        self.bag_of_words = CountVectorizer(tokenizer=self.tokenizer, ngram_range=(1,1))
+        # NOTE - we're using BOW with TF-IDF normalization for building our vocabulary and feature extraction.
+        self.tfidf_vector = TfidfVectorizer(tokenizer=self.tokenizer)
+        return self.tfidf_vector
+
+
+
+class Transformer(TransformerMixin):
+    def transform(self, doc, **transform_params):
+        return [self.clean_text(text) for text in doc]
+
+    def fit(self, doc, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+    def clean_text(self, text):
+        return text.strip().lower()
