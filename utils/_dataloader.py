61707a4b97c379357be00e60cada016f3796ba5f
wildonion <marefimoghaddam@unixerr.com>
2021-05-02 23:58:10 +0430

    call the build method of PipeLine class in __call__ method of DataLoader class :::: preprocessed -> dataloader -> training pipeline :::: remove vectorize method

--- utils/_dataloader.py
+++ utils/_dataloader.py
@@ -4,29 +4,17 @@
 
 
 
-from ._preprocessor import Preprocessor
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from ._preprocessor import Preprocessor, Transformer
+from ._pipeline import PipeLine
 
 
 __all__ = ["DataLoader"]
 
-class DataLoader:
-    def __init__(self, preprocessed: Preprocessor, transformer: Preprocessor.Transformer):
-        self.preprocessed = preprocessed
-        self.transformer  = transformer
+class DataLoader(PipeLine):
+    def __init__(self, preprocessed: Preprocessor, transformer: Transformer):
+        args = {"preprocessed": preprocessed, "transformer": transformer}
+        super().__init__(**args)
 
 
     def __call__(self, classifier):
-        pipe = Pipeline([("cleaner", self.transformer()),
-                         ('vectorizer', self.vectorizer()),
-                         ('classifier', classifier)])
-        return pipe
-
-
-    def vectorizer(self):
-        # NOTE - in BOW we'll have frequencies, rather than just 1 or 0 for their occurrence in ngram.
-        self.bag_of_words = CountVectorizer(tokenizer=self.preprocessed.tokenizer, ngram_range=(1,1))
-        # NOTE - we're using BOW with TF-IDF normalization for building our vocabulary and feature extraction.
-        self.tfidf_vector = TfidfVectorizer(tokenizer=self.preprocessed.tokenizer)
-        return self.tfidf_vector
+        return self.build(classifier)
