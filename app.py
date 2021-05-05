22919ec4fa49f85bd1ee53e2b27ae6ba27d5e980
wildonion <marefimoghaddam@unixerr.com>
2021-05-05 18:47:50 +0430

    - remove naive_bayesian_gauss algo cause it won't work on tfidf which is a sparse matrix. It can only handle desne input cause it's based on probability distribution - complete testing process

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 
 
 import argparse, sys
-import plotly.graph_objects as go
-from utils import DataLoader, MRSADatasetPipeline, Transformer, ToDense
+from utils import DataLoader, MRSADatasetPipeline, Transformer
 from models import LogisticRegression, LinearRegression, SupportVectorMachine, RandomForest, NaiveBayesian
 
 
 # ------------ processing argument options
 # -----------------------------------------------
 parser = argparse.ArgumentParser(description='Movie Review Sentiment Analysis')
-parser.add_argument('--model', action='store', type=str, help='naive_bayesian_gauss, naive_bayesian_multinomial, support_vector_machine, random_forest, logistic_regression, linear_regression.', required=True)
+parser.add_argument('--model', action='store', type=str, help='naive_bayesian, support_vector_machine, random_forest, logistic_regression, linear_regression.', required=True)
 parser.add_argument('--train-path', action='store', type=str, help='The training data CSV file path.', required=True)
 parser.add_argument('--test-path', action='store', type=str, help='The data CSV file path to test the pre-trained model.', required=True)
 
@@ -18,7 +17,7 @@ model               = args.model
 train_path          = args.train_path
 test_path           = args.test_path
 preprocessed        = MRSADatasetPipeline(paths=[train_path])
-training_dataloader = DataLoader(preprocessed=preprocessed, transformers=[Transformer, ToDense], shuffle=True)
+training_dataloader = DataLoader(preprocessed=preprocessed, transformers=[Transformer], shuffle=True)
 # print(f"[+] preprocessed length : {len(preprocessed)}") # NOTE - testing desing pattern
 # print(f"[+] the third sample is : {preprocessed[2]}") # NOTE - testing desing pattern
 
@@ -26,10 +25,8 @@ training_dataloader = DataLoader(preprocessed=preprocessed, transformers=[Transf
 
 # ------------ training process based on selected model
 # ----------------------------------------------------------
-if model == "naive_bayesian_gauss":
-    model = NaiveBayesian(training_dataloader=training_dataloader, algorithm="gauss")
-elif model == "naive_bayesian_multinomial":
-    model = NaiveBayesian(training_dataloader=training_dataloader, algorithm="multimonial")
+if model == "naive_bayesian":
+    model = NaiveBayesian(training_dataloader=training_dataloader)
 elif model == "support_vector_machine":
     model = SupportVectorMachine(training_dataloader=training_dataloader)
 elif model == "random_forest":
@@ -46,4 +43,10 @@ model.train()
 
 # ------------ testing and statistical process based on pre-trained model
 # --------------------------------------------------------------------------
-model.stat()
+statistics = model.stat()
+print("\t[+] Accuracy : ", statistics["accuracy"])
+print("\t[+] Precision : ", statistics["precision"])
+print("\t[+] Recall : ", statistics["recall"])
+print("\t[+] f1-score : ", statistics["f1_score"])
+predicted  = model([test_path])
+print("\t[+] Predicted Label for Test Data : ", predicted)
