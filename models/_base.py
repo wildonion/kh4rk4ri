7f8d065769f217e9ade065efc2b4636086e9ecde
wildonion <marefimoghaddam@unixerr.com>
2021-05-05 18:52:25 +0430

    complete training and testing  method

--- models/_base.py
+++ models/_base.py
@@ -1,12 +1,16 @@
 
 
 
-# https://towardsdatascience.com/text-classification-using-naive-bayes-theory-a-working-example-2ef4b7eb7d5a
-# https://www.dataquest.io/blog/tutorial-text-classification-in-python-using-spacy/
 
 
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+from typing import List
+import pandas as pd
+import os
+import seaborn as sns
+
 
 
 class BaseLine:
@@ -15,16 +19,40 @@ class BaseLine:
         self.model      = kwargs["model"]
         self.dataloader.pipeline = Pipeline([("cleaner", self.dataloader.transformers[0]()),
                                              ('vectorizer', self.dataloader.dataset.vectorizer()),
-                                             ('to_dense', self.dataloader.transformers[1]()),
-                                             ('classifier', self.model)])
+                                             ('classifier', self.model)]) # preprocessed piepline
 
     def stat(self):
-        # TODO - scores and plotting
-        pass
+        """
+                    accuracy: (tp + tn) / (p + n)
+                    precision tp / (tp + fp)
+                    recall: tp / (tp + fn)
+                    f1: 2 tp / (2 tp + fp + fn)
+        """
+        predicted = self.dataloader.pipeline.predict(self.dataloader.dataset.x_test)
+        mat = confusion_matrix(self.dataloader.dataset.y_test, predicted)
+        sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels=["Positive Review", "Negative Review"], yticklabels=["Positive Review", "Negative Review"])
+        plt.xlabel("true labels")
+        plt.ylabel("predicted label")
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        cmat_path = os.path.abspath(curr_dir + f"/../utils/cmat/{self.model.__class__.__name__}.png")
+        plt.savefig(cmat_path)
+        return {
+                "accuracy" : accuracy_score(self.dataloader.dataset.y_test, predicted),
+                "precision": precision_score(self.dataloader.dataset.y_test, predicted),
+                "recall"   : recall_score(self.dataloader.dataset.y_test, predicted),
+                "f1_score" : f1_score(self.dataloader.dataset.y_test, predicted)
+                }
 
 
-    def __call__(self):
-        pass
+    def __call__(self, paths: List[str]):
+        if not os.path.exists(paths[0]): print("[?] CSV Test File Not Found!"); sys.exit(1)
+        input_test_data = pd.read_csv(f"{paths[0]}/data.csv")
+        predicted_input_test_data = self.dataloader.pipeline.predict(input_test_data["text"]) # NOTE - all preprocessing is done through the pipeline estimators we've built
+        input_test_data['label'] = predicted_input_test_data.tolist()
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        labeled_input_test_data = os.path.abspath(curr_dir + f"/../utils/labeled/{self.model.__class__.__name__}.csv")
+        input_test_data.to_csv(labeled_input_test_data, index=False)
+        return predicted_input_test_data
 
 
     def train(self):
