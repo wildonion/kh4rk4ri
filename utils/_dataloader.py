381f2258994f8f3565f2ff7d36591a7b9b04e220
wildonion <marefimoghaddam@unixerr.com>
2021-05-05 11:03:51 +0430

    add transformers list

--- utils/_dataloader.py
+++ utils/_dataloader.py
@@ -3,7 +3,8 @@
 
 
 
-from ._transformer import Transformer
+from typing import List
+from ._transformer import Transformer, ToDense
 from ._preprocessor import MRSADatasetPipeline
 from sklearn.model_selection import train_test_split
 
@@ -12,13 +13,18 @@ __all__ = ["DataLoader"]
 
 
 class DataLoader:
-    def __init__(self, dataset: MRSADatasetPipeline, transformer: Transformer, shuffle: bool):
-        self.dataset     =  dataset
-        self.transformer = Transformer
-        self.pipeline    = None
+    def __init__(self, preprocessed: MRSADatasetPipeline, transformers: List[Transformer, ToDense], shuffle: bool):
+        self.dataset      =  preprocessed
+        self.transformers = transformers
+        self.pipeline     = None
         self.dataset.x_train, self.dataset.x_test, self.dataset.y_train, self.dataset.y_test = train_test_split(self.dataset.x_train, self.dataset.y_train, test_size=0.2, shuffle=shuffle)
 
     def __call__(self):
+        pass
+
+    
+    def plot(self):
+        # TODO - dataset plotting
         pass
 
         
