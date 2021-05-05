c9bcd7190a45a3cd998ffe48e58b8144982ae2b4
wildonion <marefimoghaddam@unixerr.com>
2021-05-05 11:01:36 +0430

    pass transformers list

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 
 
-import argparse
+import argparse, sys
 import plotly.graph_objects as go
-from utils import DataLoader, MRSADatasetPipeline, Transformer
+from utils import DataLoader, MRSADatasetPipeline, Transformer, ToDense
 from models import LogisticRegression, LinearRegression, SupportVectorMachine, RandomForest, NaiveBayesian
 
 
 # ------------ processing argument options
 # -----------------------------------------------
 parser = argparse.ArgumentParser(description='Movie Review Sentiment Analysis')
-parser.add_argument('--model', action='store', type=str, help='naive_bayesian_gauss, naive_bayesian_multinomial, support_vector_machine, random_forest, logistic_regression, linear_regression ', required=True)
-parser.add_argument('--train-path', action='store', type=str, help='The training data CSV file path', required=True)
-parser.add_argument('--test-path', action='store', type=str, help='The data CSV file path to test the pre-trained model', required=True)
+parser.add_argument('--model', action='store', type=str, help='naive_bayesian_gauss, naive_bayesian_multinomial, support_vector_machine, random_forest, logistic_regression, linear_regression.', required=True)
+parser.add_argument('--train-path', action='store', type=str, help='The training data CSV file path.', required=True)
+parser.add_argument('--test-path', action='store', type=str, help='The data CSV file path to test the pre-trained model.', required=True)
 
 args                = parser.parse_args()
 model               = args.model
 train_path          = args.train_path
 test_path           = args.test_path
 preprocessed        = MRSADatasetPipeline(paths=[train_path])
-training_dataloader = DataLoader(preprocessed=preprocessed, transformer=Transformer, shuffle=True)
+training_dataloader = DataLoader(preprocessed=preprocessed, transformers=[Transformer, ToDense], shuffle=True)
+# print(f"[+] preprocessed length : {len(preprocessed)}") # NOTE - testing desing pattern
+# print(f"[+] the third sample is : {preprocessed[2]}") # NOTE - testing desing pattern
 
 
 
@@ -26,7 +28,7 @@ training_dataloader = DataLoader(preprocessed=preprocessed, transformer=Transfor
 # ----------------------------------------------------------
 if model == "naive_bayesian_gauss":
     model = NaiveBayesian(training_dataloader=training_dataloader, algorithm="gauss")
-if model == "naive_bayesian_multinomial":
+elif model == "naive_bayesian_multinomial":
     model = NaiveBayesian(training_dataloader=training_dataloader, algorithm="multimonial")
 elif model == "support_vector_machine":
     model = SupportVectorMachine(training_dataloader=training_dataloader)
