8b1323e77fd35b99dbd1e3a8cba9d9b3f40ce4ff
wildonion <marefimoghaddam@unixerr.com>
2021-05-06 13:25:20 +0430

    add saving vocab logic in __call__ method

--- utils/_dataloader.py
+++ utils/_dataloader.py
@@ -7,6 +7,7 @@ from typing import List
 from ._transformer import Transformer
 from ._preprocessor import MRSADatasetPipeline
 from sklearn.model_selection import train_test_split
+import pickle, os
 
 
 __all__ = ["DataLoader"]
@@ -20,7 +21,13 @@ class DataLoader:
         self.dataset.x_train, self.dataset.x_test, self.dataset.y_train, self.dataset.y_test = train_test_split(self.dataset.x_train, self.dataset.y_train, test_size=0.2, shuffle=shuffle)
 
     def __call__(self):
-        pass
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        vocab_path = os.path.abspath(curr_dir + f"/../utils/vocabulary/vocab.p")
+        # NOTE - method 1 : 
+        # pickle.dump(self.dataset.tfidf_vector, open(vocab_path, "wb"))
+        # NOTE - method 2 : 
+        pickle.dump(self.pipeline[1], open(vocab_path, "wb"))
+        
 
     
     def plot(self):
