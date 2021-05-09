ec419637a125cacc8befd01e883fe324a171d9a1
wildonion <marefimoghaddam@unixerr.com>
2021-05-09 20:00:35 +0430

    change default parameters in SVC

--- models/_svm.py
+++ models/_svm.py
@@ -16,5 +16,5 @@ __all__ = ["SupportVectorMachine"]
 
 class SupportVectorMachine(BaseLine):
     def __init__(self, training_dataloader: DataLoader):
-        args = {"dataloader": training_dataloader, "model": SVC()}
+        args = {"dataloader": training_dataloader, "model": SVC(kernel='rbf', C=1E5), "class_name": self.__class__.__name__} # NOTE - https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html
         super().__init__(**args)
